fa3444662a1fcb1e677402e0662f24b397deeaa4
flyher <dong3580@gmail.com>
2014-04-16 10:55:02 +0800

    build project

---
+++ ColorPicker/Main.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace ColorPicker
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.txtColor = new System.Windows.Forms.TextBox();
+            this.pbColor = new System.Windows.Forms.PictureBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pbColor)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(165, 124);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 7;
+            this.btnStop.Text = "Stop(E)";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // txtColor
+            // 
+            this.txtColor.Location = new System.Drawing.Point(165, 12);
+            this.txtColor.Name = "txtColor";
+            this.txtColor.Size = new System.Drawing.Size(91, 21);
+            this.txtColor.TabIndex = 6;
+            this.txtColor.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.txtColor_MouseDoubleClick);
+            // 
+            // pbColor
+            // 
+            this.pbColor.Location = new System.Drawing.Point(12, 12);
+            this.pbColor.Name = "pbColor";
+            this.pbColor.Size = new System.Drawing.Size(138, 138);
+            this.pbColor.TabIndex = 5;
+            this.pbColor.TabStop = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(165, 80);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Start(P)";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(165, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "双击复制";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(281, 170);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.txtColor);
+            this.Controls.Add(this.pbColor);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ColorPicker";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Main_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pbColor)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.TextBox txtColor;
+        private System.Windows.Forms.PictureBox pbColor;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ ColorPicker/Main.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ColorPicker
+{
+    public partial class Main : Form
+    {
+        /// <summary>
+        /// build:2014-02-27
+        /// update:2014-04-16
+        /// </summary>
+        public Main()
+        {
+            InitializeComponent();
+        }
+        private void Main_Load(object sender, EventArgs e)
+        {
+            btnStart.Text = "Start(&p)";
+            btnStop.Text = "Stop(&e)";
+        }
+        private void txtColor_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Clipboard.SetDataObject(txtColor.Text.Trim());
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            timer1.Interval = 50;
+            timer1.Start();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            timer1.Enabled = false;
+        }
+
+        private void Main_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.P)
+            {
+                btnStart_Click(sender, e);
+            }
+            if (e.KeyCode == Keys.E)
+            {
+                btnStop_Click(sender, e);
+            }
+        }
+        Color c;
+        string color;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Dictionary<Color, string> dic_Color = GetColor();
+
+            foreach (var item in dic_Color)
+            {
+                c = item.Key;
+                color = item.Value;
+            }
+            pbColor.BackColor = c;
+            txtColor.Text = color.ToUpper();
+        }
+        #region 内部方法
+        /*屏幕取色*/
+        public Dictionary<Color, string> GetColor()
+        {
+            int r, g, b;
+            Point p = Control.MousePosition;    //得到当前鼠标坐标 
+            Color c = GetScrPixel(p);           //取色方法，传参p 当前坐标
+            r = c.R;
+            g = c.G;
+            b = c.B;
+            string res = "#" + (Convert.ToString(r, 16) == "0" ? "00" : Convert.ToString(r, 16)) + (Convert.ToString(g, 16) == "0" ? "00" : Convert.ToString(g, 16)) + (Convert.ToString(r, 16) == "0" ? "00" : Convert.ToString(b, 16));  //rgb文本框写的内容
+            Dictionary<Color, string> dic_Color = new Dictionary<Color, string>();
+            dic_Color.Add(c, res);
+            //System.GC.Collect();        //内存垃圾回收
+            return dic_Color;
+        }
+        /// <summary>
+        /// 取色方法
+        /// </summary>
+        /// <param name="pt">坐标</param>
+        /// <returns>返回颜色</returns>
+        private static Color GetScrPixel(Point pt)
+        {
+            var scrBound = Screen.PrimaryScreen.Bounds;
+            using (var bmp = new Bitmap(scrBound.Width, scrBound.Height))
+            {
+                using (var g = Graphics.FromImage(bmp))
+                {
+                    g.CopyFromScreen(scrBound.Location, scrBound.Location, scrBound.Size);
+                }
+                System.GC.Collect();
+                return bmp.GetPixel(pt.X, pt.Y);
+            }
+        }
+        #endregion
+
+
+    }
+}
